fe159122cd65ec879b3fbb9faa317f9f00985989
Pritom Karmokar <pritomkarmokar99@gmail.com>
2025-04-11 19:02:19 +0600

    update: urls

--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -9,11 +9,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Blogs API",
         default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        description="Blogs API",
         license=openapi.License(name="BSD License"),
     ),
     public=True,
